c7b5e7b4a9f698b784d7915438953d126e4ddae3
Leandro Siqueira Silva <Leandross2006@gmail.com>
2024-12-23 16:13:20 -0500

    Changes colour of car when package has been picked up

--- DeliveryDriver/Assets/Delivery.cs
+++ DeliveryDriver/Assets/Delivery.cs
@@ -1,11 +1,19 @@
 using Unity.VisualScripting;
+using UnityEditor.U2D;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float destroyDelay;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
 
     bool hasPackage;
-    [SerializeField] float destroyDelay;
+    SpriteRenderer spriteRenderer;
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Collision detected");
@@ -15,12 +23,14 @@ public class Delivery : MonoBehaviour
         if(other.tag == "Package" && !hasPackage) {
             Debug.Log("Package picked up");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
         }
 
         if(other.tag == "Customer" && hasPackage) {
             Debug.Log("Package delivered");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }

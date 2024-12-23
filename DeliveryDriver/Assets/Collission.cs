78f814bfb88d632dc43b3e00a4d9872ab6c9d342
Leandro Siqueira Silva <Leandross2006@gmail.com>
2024-12-22 22:05:50 -0500

    Added basic trigger detection

--- DeliveryDriver/Assets/Collission.cs
+++ DeliveryDriver/Assets/Collission.cs
@@ -5,4 +5,8 @@ public class Collission : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Collision detected");
     }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Trigger triggered");
+    }
 }

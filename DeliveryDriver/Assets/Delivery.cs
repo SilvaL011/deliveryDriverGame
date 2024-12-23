f804460f1adb06de728980964ccf768554613c3c
Leandro Siqueira Silva <Leandross2006@gmail.com>
2024-12-23 16:01:43 -0500

    Makes packages disappear and does not let car pick up more than 1 package at a time

--- DeliveryDriver/Assets/Delivery.cs
+++ DeliveryDriver/Assets/Delivery.cs
@@ -1,18 +1,26 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+
+    bool hasPackage;
+    [SerializeField] float destroyDelay;
+
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Collision detected");
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Package") {
+        if(other.tag == "Package" && !hasPackage) {
             Debug.Log("Package picked up");
+            hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         }
 
-        if(other.tag == "Customer") {
+        if(other.tag == "Customer" && hasPackage) {
             Debug.Log("Package delivered");
+            hasPackage = false;
         }
     }
 }
